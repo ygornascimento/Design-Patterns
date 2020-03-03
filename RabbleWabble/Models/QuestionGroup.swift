721062ca0b70e24531519b0a45bbe3b6b79d40fb
Ygor Nascimento <nascimento.ygor@outlook.com>
2020-03-03 06:50:44 -0300

    Fixing error in Chapter 8: Observer Pattern

--- RabbleWabble/Models/QuestionGroup.swift
+++ RabbleWabble/Models/QuestionGroup.swift
@@ -6,11 +6,11 @@
 //  Copyright © 2020 Ygor Nascimento. All rights reserved.
 //
 
-import Foundation
 import Combine
+import Foundation
 
 public class QuestionGroup: Codable {
-    
+
     public class Score: Codable {
         public var correctCount: Int = 0 {
             didSet { updateRunningPercentage() }
@@ -18,23 +18,22 @@ public class QuestionGroup: Codable {
         public var incorrectCount: Int = 0 {
             didSet { updateRunningPercentage() }
         }
-        
         @Published public var runningPercentage: Double = 0
-        
-        public init () {}
-        
+
+        public init() { }
+
         private enum CodingKeys: String, CodingKey {
             case correctCount
-            case incorrecCount
+            case incorrectCount
         }
-        
+
         public required init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             self.correctCount = try container.decode(Int.self, forKey: .correctCount)
-            self.incorrectCount = try container.decode(Int.self, forKey: .incorrecCount)
+            self.incorrectCount = try container.decode(Int.self, forKey: .incorrectCount)
             updateRunningPercentage()
         }
-        
+
         private func updateRunningPercentage() {
             let totalCount = correctCount + incorrectCount
             guard totalCount > 0 else {
@@ -43,21 +42,22 @@ public class QuestionGroup: Codable {
             }
             runningPercentage = Double(correctCount) / Double(totalCount)
         }
-        
+
         public func reset() {
             correctCount = 0
             incorrectCount = 0
         }
     }
-    
+
     public let questions: [Question]
     public private(set) var score: Score
     public let title: String
-    
-    public init(questions: [Question], score: Score = Score(), title: String) {
+
+    public init(questions: [Question],
+                score: Score = Score(),
+                title: String) {
         self.questions = questions
         self.score = score
         self.title = title
     }
-    
 }
